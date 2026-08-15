5685d2714ec84701f4a74d4dd20b3b2c267798fc
Aleksandr <AARomanov1985@yandex.ru>
2026-08-15 21:16:18 +0200

    fix: VHS player bug

--- vhs_player/budget_funai/filter.sh
+++ vhs_player/budget_funai/filter.sh
@@ -2,8 +2,8 @@
 # video heads drift slowly, the tracking wanders, dropout chatters on the
 # head-switch band, FM noise rides old heads. Mono audio on a loud hiss bed.
 
-DECK_WAND="if(lt(abs(Y-mod(N*0.4,540)),9),0.86,1)"
-DECK_DTC="if(lt(abs(Y-mod(N*1.7,540)),5),lum(X,Y)*0.4,lum(X,Y))"
+DECK_WAND="1-0.12*exp(-pow((Y-mod(N*0.4,540)+3*(random(1)-0.5))/6,2))*(0.7+0.3*random(2))"
+DECK_DTC="lum(X,Y)*(1-0.25*exp(-pow((Y-(566+4*random(5)))/3,2)))+exp(-pow((Y-(566+4*random(5)))/3,2))*random(7)*90"
 DECK_NOISE=5
 DECK_AMP=0.0025
 DECK_HIGHPASS=140
